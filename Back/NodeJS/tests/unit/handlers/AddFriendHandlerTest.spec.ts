4f812d41408dbdd4c72ed006318e42c41a860d4a
steve valentin <s.valentin77@hotmail.fr>
2024-06-24 23:45:57 +0200

    clean

--- Back/NodeJS/tests/unit/handlers/AddFriendHandlerTest.spec.ts
+++ Back/NodeJS/tests/unit/handlers/AddFriendHandlerTest.spec.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from 'vitest';
+import { describe, test } from 'vitest';
 import  {v4 as uuidv4} from 'uuid';
 import { AddFriendSut } from '../SUTs/AddFriendSut';
 
