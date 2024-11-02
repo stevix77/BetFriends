7f2f5f58660695cd81a5b40a62184493488c6918
steve valentin <s.valentin77@hotmail.fr>
2024-11-02 11:51:21 +0100

    angular improve ui display bets

--- Front/TS/src/infrastructure/ui/Angular/src/app/bets/BetsViewModel.ts
+++ Front/TS/src/infrastructure/ui/Angular/src/app/bets/BetsViewModel.ts
@@ -13,10 +13,34 @@ export class BetsViewModel {
                 Coins: x.Coins,
                 Description: x.Description,
                 EndDate: x.EndDate.toString(),
-                OwnerId: x.BookieId,
-                OwnerName: x.OwnerName
+                BookieId: x.BookieId,
+                BookieName: x.OwnerName,
+                InvitedCount: x.Gamblers.length,
+                AcceptedCount: x.Gamblers.filter(x => x.HasAccepted).length
             }
         })
+    }
+
+    async Accept(betId: string): Promise<void>{
+        const bet = this.GetBetById(betId);
+        if(!bet) {
+            return;
+        }
+
+        await this.betController.AnswerAsync(bet.Id, true, new Date(bet.EndDate), bet.BookieId)
+    }
+
+    async Decline(betId: string): Promise<void>{
+        const bet = this.GetBetById(betId);
+        if(!bet) {
+            return;
+        }
+
+        await this.betController.AnswerAsync(betId, false, new Date(bet.EndDate), bet.BookieId)
+    }
+
+    private GetBetById(betId: string): BetDto|undefined {
+        return this.Bets.find(x => x.Id == betId);
     }
 
 }
@@ -26,6 +50,8 @@ export interface BetDto {
     Description: string;
     EndDate: string;
     Coins: number;
-    OwnerId: string;
-    OwnerName: string;
+    BookieId: string;
+    BookieName: string;
+    AcceptedCount: number;
+    InvitedCount: number;
 }
