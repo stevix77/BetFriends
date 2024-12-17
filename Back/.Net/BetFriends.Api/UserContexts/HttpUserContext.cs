5ba0318500677dbc560cc3477841d21cb8fdaf51
steve valentin <s.valentin77@hotmail.fr>
2024-12-17 09:39:17 +0100

    dotnet : add httpUserContext

---
+++ Back/.Net/BetFriends.Api/UserContexts/HttpUserContext.cs
@@ -0,0 +1,16 @@
+﻿using BetFriends.Bets.Application.Abstractions;
+
+namespace BetFriends.Api.UserContexts;
+
+public class HttpUserContext(IHttpContextAccessor httpContextAccessor) : IUserContext
+{
+    public Guid UserId
+    {
+        get
+        {
+            if(Guid.TryParse(httpContextAccessor.HttpContext?.Request.Headers.Authorization, out Guid id))
+                return id;
+            return Guid.Empty;
+        }
+    }
+}
--- Back/.Net/BetFriends.Api/UserContexts/StubUserContext.cs
+++ Back/.Net/BetFriends.Api/UserContexts/StubUserContext.cs
@@ -1,8 +1,8 @@
 ﻿using BetFriends.Bets.Application.Abstractions;
 
-namespace BetFriends.Api;
+namespace BetFriends.Api.UserContexts;
 
-public class UserContext : IUserContext
+public class StubUserContext : IUserContext
 {
     public Guid UserId => Guid.Parse("11111111-1111-1111-1111-111111111111");
 
