0e894aa788538754e91c57b832a0ab49989b973c
steve valentin <s.valentin77@hotmail.fr>
2025-01-06 00:23:56 +0100

    front TS : add bearer token

--- Front/TS/src/infrastructure/ui/Angular/src/app/services/httpService.ts
+++ Front/TS/src/infrastructure/ui/Angular/src/app/services/httpService.ts
@@ -1,21 +1,40 @@
-import { HttpClient, HttpResponse } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
 import { HttpObject, IHttpService } from "../../../../../adapters/http/IHttpService";
 import { lastValueFrom } from "rxjs";
+import { AuthService } from "./authService";
 
 export class HttpService implements IHttpService {
     private readonly apiUrl: string;
+    private authToken?: string;
 
-    constructor(private readonly httpClient: HttpClient, apiUrl: string) {
+    constructor(private readonly httpClient: HttpClient, 
+                apiUrl: string, 
+                private readonly authService: AuthService) {
         this.apiUrl = apiUrl;
+        this.authService.isAuthenticated$.subscribe(async (status) => {
+            if(status) {
+                this.authToken = localStorage.getItem('auth_token')!;
+                return;
+            }
+            this.authToken = undefined;
+        })
     }
     async Get(url: string): Promise<HttpObject> {
-        const observable = this.httpClient.get(`${this.apiUrl}/${url}`, { observe: 'response' });
+        const headers = new HttpHeaders({
+            Authorization: `Bearer ${this.authToken}`,
+            'Content-Type': 'application/json'
+        });
+        const observable = this.httpClient.get(`${this.apiUrl}/${url}`, { observe: 'response', headers: headers });
         const response = await lastValueFrom(observable);
         return this.HandleResponse(response)
     }
     
     async Post(url: string, data?: any): Promise<HttpObject> {
-        const observable = this.httpClient.post(`${this.apiUrl}/${url}`, data, { observe: 'response' });
+        const headers = new HttpHeaders({
+            Authorization: `Bearer ${this.authToken}`,
+            'Content-Type': 'application/json'
+        });
+        const observable = this.httpClient.post(`${this.apiUrl}/${url}`, data, { observe: 'response', headers: headers });
         const response = await lastValueFrom(observable);
         return this.HandleResponse(response)
     }
--- Front/TS/src/infrastructure/ui/Vue/src/services/httpService.ts
+++ Front/TS/src/infrastructure/ui/Vue/src/services/httpService.ts
@@ -1,19 +1,29 @@
 import { type IHttpService, type HttpObject } from "../../../../adapters/http/IHttpService";
 import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
+import type { AuthService } from "./authService";
 
 export class HttpService implements IHttpService {
     private readonly apiClient: AxiosInstance;
-    constructor(apiUrl: string) {
+    private authToken?: string;
+    constructor(apiUrl: string, private readonly authService: AuthService) {
         this.apiClient = axios.create({
             baseURL: apiUrl,
             headers: {
                 'Content-Type': 'application/json'
             }
         })
+        this.authService.isAuthenticated$.subscribe(async (status) => {
+            if(status) {
+                this.authToken = localStorage.getItem('auth_token')!;
+                return;
+            }
+            this.authToken = undefined;
+        })
     }
         
 
     async Get(url: string): Promise<HttpObject> {
+        this.apiClient.defaults.headers.common["Authorization"] = `Bearer ${this.authToken}`
         const response = await this.apiClient.get(url);
         if(response.status >= 200 || response.status <= 299) {
             return this.HandleSuccess(response)
@@ -23,6 +33,7 @@ export class HttpService implements IHttpService {
     }
     
     async Post(url: string, data?: any): Promise<HttpObject> {
+        this.apiClient.defaults.headers.common["Authorization"] = `Bearer ${this.authToken}`
         const response = await this.apiClient.post(url, data);
         if(response.status >= 200 || response.status <= 299) {
             return this.HandleSuccess(response)
